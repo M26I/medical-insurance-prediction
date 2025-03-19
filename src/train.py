4232fc665e714559e537de084889074c9e08ca58
M26I <marija.ivanisevic267@gmail.com>
2025-03-19 07:50:50 +0000

    paths fixed

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 import matplotlib
@@ -8,38 +9,45 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-data = pd.read_csv("C:/Users/marij/Desktop/medical-insurance-prediction/data/insurance.csv")
+# Get base directory of script
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-#OHE
+# Load dataset using a relative path
+data_path = os.path.join(BASE_DIR, "..", "data", "insurance.csv")
+data = pd.read_csv(data_path)
+
+#  (OHE)
 data = pd.get_dummies(data, drop_first=True)
 
-#Features and target
-X= data.drop("charges", axis=1)
+# features
+X = data.drop("charges", axis=1)
 y = data["charges"]
 
-#Split into train and test sets
-X_train, X_test, y_train, y_test =train_test_split(X, y, test_size=0.2, random_state=42)
+# split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-#train
+# Train
 model = LinearRegression()
 model.fit(X_train, y_train)
 
+# Predictions
 y_pred = model.predict(X_test)
 
-#evaluation
-
+# Evaluation metrics
 mae = mean_absolute_error(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 
 print(f"Mean Absolute Error (MAE): {mae:.2f}")
 print(f"Mean Squared Error (MSE): {mse:.2f}")
 
-#plot
-
+#  plot
 plt.figure(figsize=(8,6))
 sns.scatterplot(x=y_test, y=y_pred, alpha=0.6)
 plt.xlabel("Actual Charges")
 plt.ylabel("Predicted Charges")
 plt.title("Actual vs Predicted Medical Charges")
-plt.savefig("C:/Users/marij/Desktop/medical-insurance-prediction/reports/actual_vs_predicted.png")
+
+# Save plot using a relative path
+reports_path = os.path.join(BASE_DIR, "..", "reports", "actual_vs_predicted.png")
+plt.savefig(reports_path)
 plt.show()
